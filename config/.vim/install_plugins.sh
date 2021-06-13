75b78432f00ea625d01f7f347c8a48c4cc916e3e
deriando <deriandogoh@outlook.com>
2021-06-14 04:06:32 +0800

    add: vim

---
+++ config/.vim/install_plugins.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/pack/dist/start/vim-airline/
+git clone https://github.com/airblade/vim-gitgutter.git ~/.vim/pack/dist/start/vim-gitgutter/
+# git clone https://github.com/tpope/vim-fugitive.git ~/.vim/pack/dist/start/vim-fugitive/
